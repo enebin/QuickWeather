585d282e3fbefaf0805058b2bb3ef360c36e5b31
enebin <lyb35548@gmail.com>
2022-02-11 17:00:28 +0900

    Add: Map marker

--- QuickWeather/LocalWeather/MapView.swift
+++ QuickWeather/LocalWeather/MapView.swift
@@ -8,18 +8,31 @@
 import SwiftUI
 import MapKit
 
+struct Marker: Identifiable {
+    let id = UUID()
+    var location: MapMarker
+}
+
+
 class MapViewModel: ObservableObject {
     @Published var region = MKCoordinateRegion(
         center: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914),
         span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    @Published var markers: [Marker] = [Marker(location: MapMarker(coordinate: CLLocationCoordinate2D(latitude: 37.5666791, longitude: 126.9782914), tint: .red))]
 }
 
 struct MapView: View {
     @ObservedObject var viewModel: MapViewModel
-
+    
     var body: some View {
-        Map(coordinateRegion: $viewModel.region, interactionModes: [.zoom])
+        Map(coordinateRegion: $viewModel.region, interactionModes: [.zoom], annotationItems: viewModel.markers) {
+            marker in
+            marker.location
+        }
+        
     }
+    
 
     init(location: CLLocationCoordinate2D) {
         let region = MKCoordinateRegion(
@@ -29,6 +42,7 @@ struct MapView: View {
         
         viewModel = MapViewModel()
         viewModel.region = region
+        viewModel.markers = [Marker(location: MapMarker(coordinate: location, tint: .red))]
         print("Region: \(viewModel.region)")
     }
 }
